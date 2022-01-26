68ab79161b7ce581e103fae2cbb62c33a0501bed
Matt Walsh <github@netbymatt.com>
2022-01-26 16:23:39 -0600

    remove internal console.log

--- src/draw/palettize.js
+++ src/draw/palettize.js
@@ -6,7 +6,6 @@ const palettizeImage = (sourceCtx, palette) => {
 		x: sourceCtx.canvas.width,
 		y: sourceCtx.canvas.height,
 	};
-	console.time('Palettize image');
 
 	// transform into indexed palette by finding the closest indexed color for each pixel
 	// create a destination image that is transparent and indexed
@@ -19,11 +18,10 @@ const palettizeImage = (sourceCtx, palette) => {
 	const indexedImageData = indexedCtx.getImageData(0, 0, dim.x, dim.y);
 
 	// loop through each pixel
-	console.log(`Palettized cache size: ${Object.keys(palette.closest).length}`);
 	indexedImageData.data.forEach((val, idx) => {
 		indexedImageData.data[idx] = palette.closestIndex(sourceImageData.data.slice(idx * 4, idx * 4 + 3));
 	});
-	console.timeEnd('Palettize image');
+
 	// write the new image data
 	indexedCtx.putImageData(indexedImageData, 0, 0);
 
