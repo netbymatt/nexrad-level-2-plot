2017a6f4ffd2680b353c37aae4715b3cc91ff4c7
Matt Walsh <github@netbymatt.com>
2022-03-11 09:53:14 -0600

    fix benchmark code

--- demos/benchmark.js
+++ demos/benchmark.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-await-in-loop */
 const { Level2Radar } = require('nexrad-level-2-data');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const glob = require('glob');
@@ -20,28 +21,26 @@ files.forEach((file) => {
 // combine data
 const radarData = Level2Radar.combineData(chunks);
 
-// result array by elevations
-const plots = [];
-
 const size = 1800;
-const elevation = 1;
 
 // plot for each elevation and size
 (async () => {
 	const start = new Date();
 	for (let i = 0; i < 100; i += 1) {
-		plots[elevation] = plot(radarData, ['REF', 'VEL'], {
-			elevation,
+		const plots = plot(radarData, ['REF', 'VEL'], {
 			size,
 			palettize: true,
 			cropTo: size / 2,
 		});
 
 		// write files to disk
-		await Promise.allSettled([
-			writePngToFile(`./output/REF-${elevation}-${size}--${i}.png`, plots[elevation].REF),
-			writePngToFile(`./output/VEL-${elevation}-${size}--${i}.png`, plots[elevation].VEL),
-		]);
+		const writePromises = [];
+		plots.forEach((p) => {
+			const { elevation } = p;
+			writePromises.push(writePngToFile(`./output/REF-${elevation}-${size}.png`, p.REF));
+			writePromises.push(writePngToFile(`./output/VEL-${elevation}-${size}.png`, p.VEL));
+		});
+		await Promise.allSettled(writePromises);
 	}
 	const end = new Date();
 	console.log(`Total Time: ${(end - start) / 1000}`);
