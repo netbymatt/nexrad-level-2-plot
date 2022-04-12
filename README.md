7dd5ba62215b8e8887d4941088c9ba7afe8fce06
Matt Walsh <github@netbymatt.com>
2022-04-12 09:26:19 -0500

    begin jsdoc documentation

--- README.md
+++ README.md
@@ -46,39 +46,7 @@ Test code and data is provided in the `./demo` folder. `test.js` can be used to 
 
 # API
 
-## plot(data, products, options)
-Returns an array of objects. The order of elevations in the returned array matches the order of elevations in options.elevations
-``` javascript
-	[
-		{
-			elevation: <integer>,
-			REF: {
-				canvas: <Canvas>,
-				palette: <Uint8ClampedArray>
-			},
-			//... additional products
-		},
-	// ... additional elevations
-	]
-```
-The first level key in the returned object represents each requested product.
-> Note: Each product may return false if no data was found for the specified elevation and product.
 
-```[canvas](https://www.npmjs.com/package/canvas)``` contains the plotted data and functions for outputtting various image formats.
-
-```palette``` is an array that can be used to create palettized images from the canvas library.
-
-|Paramater|Type|Default|Description|
-|---|---|---|---|
-data|Level2Radar||Output from [nexrad-level-2-data](https://github.com/netbymatt/nexrad-level-2-data/).
-products|Array of strings|All|Individual strings as shown in supported products to specify the products to be plotted.
-options.size|integer|3600|1 to 3600. Size of the x and y axis in pixels. The image must be square so only a single integer is needed. See [downsampling](#downsampling)
-options.cropTo|integer|3600|1 to 3600. After scaling and downsampling as described above crop the resulting plot to the size specified. Internally, the image is actually drawn at the cropped size to save on processing time.
-options.background|string|#000000|Background color of the image. This can be transparent by using #RGBA notation. See [ctx.fillStyle](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillStyle) for more information.
-options.lineWidth|integer|2|The raster image is created by drawing several arcs at the locations and colors specified in the data file. When scaling down you may get a better looking image by adjusting this value to something large than the default.
-options.palettize|boolean\|object|false|After drawing the image convert the image from RGBA to a palettized image. When true the same palette as the product is used. Additional options are described in [palettizing](#palettizing). This can significantly reduce the size of the resulting image with minimal loss of clarity.
-options.elevations|undefined\|integer\|Array of integers|undefined|The specific elevations to plot. If undefined (default) all available elevations will be plotted. If an integer is provided a single elevation will be plotted with all the selected ```products```. If an array of integers are provided each ```products``` in each elevation will be plotted. Note that when a product and elevation combination do not exist the returned object will have false in the missing location. This is common where elevation 1 will have reflectivity data and elevation 2 (altough at the same angle as elevation 1) will contain velocity data.
-options.usePreferredWaveforms|boolean|true|Waveform types of 1, 2, 3, 4 and 5 are produced by the radar. Type 1 is typically exclusive to reflectivity, Type 2 is typically exclusive to velocity however it also produces reflectivity data. Types 3, 4 and 5 all produce both velocity and reflectivity. When set to true type 1 waveforms will not plot velocity data, and type 2 waveforms will not plot reflectivity data. If you must see all information set this to false.
 
 ### Downsampling
 A full size plot is 3600 x 3600 pixels. This corresponds to the maximum range of the radar 460km (~250 mi) * maximum resolution 0.25 mi/bin * 2 (east and west side of radar).
--- src/index.js
+++ src/index.js
@@ -1,32 +1,46 @@
 const { draw, canvas } = require('./draw');
 const { writePngToFile } = require('./utils/file');
 /**
- * Plot level 2 data
- * @param {Level2Data} data output from the nexrad-level-2-data library
- * @param {(string|string[])} _products Options are REF, VEL, SW , ZDR, PHI and RHO
- * @param {object} options Plotting options
- * @param {number} [options.size=3600] 1 to 3600. Size of the x and y axis in pixels. The image must be square so only a single integer is needed.
- * @param {number} [options.cropTo=3600] 1 to 3600. After scaling and downsampling as described above crop the resulting plot to the size specified. Internally, the image is actually drawn at the cropped size to save on processing time.
- * @param {string} [options.background=#000000] Background color of the image. This can be transparent by using #RGBA notation.
- * @param {number} [options.lineWidth=2] The raster image is created by drawing several arcs at the locations and colors specified in the data file. When scaling down you may get a better looking image by adjusting this value to something large than the default.
- * @param {(boolean|object)} [options.palettize] After drawing the image convert the image from RGBA to a palettized image. When true the same palette as the product is used.
- * @param {(undefined|number|number[])} [options.elevations=undefined] List of elevations to plot.
- * @param {boolean} [options.usePreferredWaveforms = true] Limit the plotted data to the preferred use of waveforms 1 and 2.
- * @returns Canvas
+ *
+ * @typedef PlottedCanvases Requested plots. Each PlottedCanvas may return false if the plot was not requested or if the data was unavailable for the plot.
+ * @property {number} elevation Elevation number
+ * @property {PlottedCanvas} [REF] Reflectivity
+ * @property {PlottedCanvas} [VEL] Velocity
  */
-const plot = (data, _products, options) => {
+/**
+ * @typedef PlottedCanvas Canvas and additional metadata
+ * @property {Canvas} canvas [canvas](https://www.npmjs.com/package/canvas) contains the plotted data and functions for outputtting various image formats.
+ * @property {Uint8ClampedArray} palette Array that can be used to create palettized images from the Canvas library.
+ */
+/**
+ * Plot level 2 data
+ *
+ * @class plot
+ * @param {Level2Data} data Output from the [nexrad-level-2-data](https://github.com/netbymatt/nexrad-level-2-data) library
+ * @param {(string|string[])} products Individual strings as shown in supported products to specify the products to be plotted.
+ * @param {object} options Plotting options
+ * @param {number} [options.size=3600] 1 to 3600. 1 to 3600. Size of the x and y axis in pixels. The image must be square so only a single integer is needed.
+ * @param {number} [options.cropTo=3600] 1 to 3600. After scaling and downsampling as described above crop the resulting plot to the size specified. Internally, the image is actually drawn at the cropped size to save on processing time.
+ * @param {string} [options.background=#000000] Background color of the image. This can be transparent by using #RGBA notation. See [ctx.fillStyle](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillStyle) for more information.
+ * @param {number} [options.lineWidth=2] The raster image is created by drawing several arcs at the locations and colors specified in the data file. When scaling down you may get a better looking image by adjusting this value to something large than the default.
+ * @param {(boolean|object)} [options.palettize] After drawing the image convert the image from RGBA to a palettized image. When true the same palette as the product is used. Additional options are described in [palettizing](#palettizing). This can significantly reduce the size of the resulting image with minimal loss of clarity.
+ * @param {(undefined|number|number[])} [options.elevations=undefined] The specific elevations to plot. If undefined (default) all available elevations will be plotted. If an integer is provided a single elevation will be plotted with all the selected ```products```. If an array of integers are provided each ```products``` in each elevation will be plotted. Note that when a product and elevation combination do not exist the returned object will have false in the missing location. This is common where elevation 1 will have reflectivity data and elevation 2 (altough at the same angle as elevation 1) will contain velocity data.
+ * @param {boolean} [options.usePreferredWaveforms = true] Waveform types of 1, 2, 3, 4 and 5 are produced by the radar. Type 1 is typically exclusive to reflectivity, Type 2 is typically exclusive to velocity however it also produces reflectivity data. Types 3, 4 and 5 all produce both velocity and reflectivity. When set to true type 1 waveforms will not plot velocity data, and type 2 waveforms will not plot reflectivity data. If you must see all information set this to false.
+ * @returns {PlottedCanvases[]} Plotted canvases in the order they are requested by the input options.elevations
+ */
+const plot = (data, products, options) => {
 	// store result
 	const result = [];
 
-	let products;
+	let requestedProducts;
 	// make product list into an array
-	if (_products && Array.isArray(_products)) {
-		products = _products;
-	} else if (typeof _products === 'string') {
-		products = [_products];
+	if (products && Array.isArray(products)) {
+		requestedProducts = products;
+	} else if (typeof products === 'string') {
+		requestedProducts = [products];
 	} else {
 		// default to all products
-		products = ['REF', 'VEL', 'SW ', 'ZDR', 'PHI', 'RHO'];
+		requestedProducts = ['REF', 'VEL', 'SW ', 'ZDR', 'PHI', 'RHO'];
 	}
 
 	// get the available elevations
@@ -40,7 +54,7 @@ const plot = (data, _products, options) => {
 
 	elevations.forEach((elevation) => {
 		const elevationResult = { elevation };
-		products.forEach((product) => {
+		requestedProducts.forEach((product) => {
 			// parse and store result
 			elevationResult[product] = draw(data, {
 				...options,
